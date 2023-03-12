42c9c484f3a55ec52c84a0de93a5593b5a361eeb
Gorbai Benedek <beni147ros@gmail.com>
2023-03-12 20:01:00 +0100

    .

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const myFunction = () => {
-  function myFunction() {
-    document.getElementById("p-1").style.display = "none";
-  }
-};
+// Create the observer
+let observer = new IntersectionObserver((entries) => {
+  // Loop over the entries
+  entries.forEach((entry) => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add("anim");
+    }
+  });
+});
+
+observer.observe(document.querySelector(".p-4"));
