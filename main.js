c4236f4f1c1a41990dfac1b4955df6ed859bf223
Gorbai Benedek <beni147ros@gmail.com>
2023-03-14 12:28:50 +0100

    initial changes added

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-// Create the observer
+// function widthChangeCallback() {
+//   if (window.innerWidth < 600) {
+//     observer.observe(document.querySelector(".anim"));
+//   } else {
+
+//     observer.unobserve(document.querySelector(".anim"));
+//   }
+// }
+// window.addEventListener('resize', widthChangeCallback);
+// widthChangeCallback();
+
+
 let observer = new IntersectionObserver((entries) => {
+
   // Loop over the entries
   entries.forEach((entry) => {
     // If the element is visible
     if (entry.isIntersecting) {
       // Add the animation class
       entry.target.classList.add("anim");
+
+
     }
   });
 });
+if (window.innerWidth < 600) {
+  observer.observe(document.querySelector(".p-4"));
+} else {
+  observer.observe(document.querySelector(".p-4"));
 
-observer.observe(document.querySelector(".p-4"));
+}
+
